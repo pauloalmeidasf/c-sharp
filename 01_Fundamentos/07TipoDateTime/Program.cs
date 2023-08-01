7504886db59f6fe693c9e20ba0c52707735ae14d
Paulo Almeida Santos Ferreira <88210792+pauloalmeidasf@users.noreply.github.com>
2023-07-31 22:05:03 -0300

    Commit Aula 31/07/2023
    Commit realizado às 22:04

---
+++ 01_Fundamentos/07TipoDateTime/Program.cs
@@ -0,0 +1,79 @@
+﻿/*
+ * DateTime (Struct)
+ * 
+ * Representa data e hora
+ * 
+ * Tipo: Valor
+ * Armazendo na memória Stack
+ * 
+ * Valor padrão: 01/01/0001 00:00:00
+ * 
+ * Referência: https://learn.microsoft.com/pt-br/dotnet/api/system.datetime?view=net-7.0
+ * 
+ */
+
+using System.Globalization;
+
+Console.WriteLine("Tipo DateTime");
+Console.WriteLine();
+
+//Define data com o padrão yyyy-MM-dd
+DateTime dataEvento = new DateTime(2023,07,31);
+
+Console.WriteLine("Data do evento: " + dataEvento.ToShortDateString());
+Console.WriteLine();
+
+//Define data com o padrão yyyy-MM-dd HH:mm:ss
+DateTime dataHoraAula = new DateTime(2023, 07, 31, 07, 15, 00);
+
+Console.WriteLine("Data Hora da Aula: " + dataHoraAula);
+Console.WriteLine();
+
+Console.WriteLine("Operações com data e hora");
+Console.WriteLine();
+
+DateTime dataAtual = DateTime.Now;
+
+Console.WriteLine("Data e hora atuais: " + dataAtual);
+Console.WriteLine("Ano: " + dataAtual.Year);
+Console.WriteLine("Mês: " + dataAtual.Month);
+Console.WriteLine("Dia: " + dataAtual.Day);
+Console.WriteLine("Horas: " + dataAtual.Hour);
+Console.WriteLine("Minutos: " + dataAtual.Minute);
+Console.WriteLine("Segundos: " + dataAtual.Second);
+Console.WriteLine("Milissegundos: " + dataAtual.Millisecond);
+
+Console.WriteLine();
+
+//adicionar valores
+
+Console.WriteLine("Data Hora atuais + 30 dias: " + dataAtual.AddDays(30));
+Console.WriteLine("Data Hora atuais + 45 horas: " + dataAtual.AddHours(45));
+Console.WriteLine("Data Hora atuais + 2 meses: " + dataAtual.AddMonths(2));
+Console.WriteLine("Data Hora atuais + 5 anos: " + dataAtual.AddYears(5));
+
+Console.WriteLine();
+
+//obter dia da semana e do ano
+
+Console.WriteLine("Dia da semana: " + dataAtual.DayOfWeek); //mostra na cultura inglesa
+Console.WriteLine("Dia da semana: " + dataAtual.ToString("dddd")); //mostra na cultura local do dispositivo
+Console.WriteLine("Dia do ano: " + dataAtual.DayOfYear);
+
+Console.WriteLine();
+
+//exibir data nos formatos longo e abreviado
+Console.WriteLine("Data Extensa: " + dataAtual.ToLongDateString());
+Console.WriteLine("Data Abreviada: " + dataAtual.ToShortDateString());
+
+Console.WriteLine();
+
+//exibir hora nos formatos longo e abreviado
+Console.WriteLine("Hora Extensa: " + dataAtual.ToLongTimeString());
+Console.WriteLine("Hora Abreviada: " + dataAtual.ToShortTimeString());
+
+Console.WriteLine();
+
+Console.WriteLine("Pressione qualquer tecla para finalizar...");
+
+Console.ReadKey();
---
+++ 01_Fundamentos/08NullableTypes/Program.cs
@@ -0,0 +1,98 @@
+﻿/*
+ * Nullable Types (Tipos anuláveis)
+ * 
+ * Uma variável de tipo-valor (int, double, char, bool, etc) não pode conter o valor null
+ * 
+ * Nullable Types permitem atribuir um valor null a um tipo-valor
+ * 
+ * Null: valor ausente ou indefinido
+ * 
+ */
+
+Console.WriteLine("Nullable Types");
+Console.WriteLine();
+
+//não é possível atribuir null a um tipo-valor; exemplo:  int
+
+//int valor = null;
+
+//Declarar Nullable Types
+Nullable<int> a = null;
+Nullable<int> b = 100;
+Nullable<double> c = 100.9;
+Nullable<double> d = null;
+Nullable<bool> e = false;
+Nullable<bool> f = null;
+
+//Declarar Nullable Types - Alternativa
+
+int? varA = null;
+int? varB = 100;
+double? varC = null;
+double? varD = 50.954;
+bool? varE = false;
+bool? varF= null;
+
+Console.WriteLine("Valor de varA: " + varA);
+Console.WriteLine("Valor de varB: " + varB);
+Console.WriteLine("Valor de varC: " + varC);
+Console.WriteLine("Valor de varD: " + varD);
+Console.WriteLine("Valor de varE: " + varE);
+Console.WriteLine("Valor de varF: " + varF);
+
+Console.WriteLine();
+
+//não é possível atribui uma variável de tipo anulável em uma variável de tipo-valor
+
+//int? h = null;
+//int i = h;
+
+Console.WriteLine("Coalescência Nula");
+Console.WriteLine();
+
+/*
+ * Coalescência Nula (??)
+ * 
+ * int? h = null
+ * int i = h ?? -1
+ * 
+ * Se o valor de 'h' for diferente de null, i receberá o valor do operando esquerdo do operador ??
+ * Se o valor de 'h' for igual a null, i receberá o valor do operando direito do operador ??
+ * 
+ */
+
+int? h = null;
+int i = h ?? -1;
+
+Console.WriteLine("Valor da variável 'i': " + i);
+
+//exemplo de coalescência nula usando string (tipo-referência)
+
+string curso = "Curso de CSharp - IMPACTA";
+string resposta = curso ?? "Não informado";
+
+Console.WriteLine("Nome do curso: " + resposta);
+Console.WriteLine();
+
+//Coalescência Nula - Operadores
+
+Console.WriteLine("Coalescência Nula - Operadores");
+Console.WriteLine();
+
+//int? valorA = 4;
+//int valorB = 3;
+//int valorC = valorA * valorB;
+
+//int? valorA = 4;
+//int? valorB = 3;
+//int valorC = valorA * valorB;
+
+int? valorA = 4;
+int? valorB = 3;
+int? valorC = valorA * valorB;
+
+Console.WriteLine("Valor da variável 'valorC': " + valorC);
+Console.WriteLine();
+
+Console.WriteLine("Pressione qualque tecla para finalizar...");
+Console.ReadKey();
