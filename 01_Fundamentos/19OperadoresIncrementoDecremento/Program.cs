de92d6d9034fddd5bf1a7c2bbc587b055810fe11
Paulo Almeida Santos Ferreira <88210792+pauloalmeidasf@users.noreply.github.com>
2023-08-02 22:16:59 -0300

    Commit Aula 02/08/2023
    Commit realizado às 22:16

---
+++ 01_Fundamentos/19OperadoresIncrementoDecremento/Program.cs
@@ -0,0 +1,65 @@
+﻿/*
+ * Operadores incrementais e decrementais
+ * 
+ * Aumentam ou diminuem em 1 unidade o valor de uma variável
+ * 
+ * Pós Incremento: x++
+ * Pré Incremento: ++x
+ * 
+ * Pós Decremento: x--
+ * Pré Decremento: --x
+ * 
+ * Referência: https://learn.microsoft.com/pt-br/dotnet/csharp/language-reference/operators/arithmetic-operators
+ * 
+ */
+
+Console.WriteLine("Operadores de Incremento e Decremento");
+Console.WriteLine();
+
+//pós incremento: primeiro resolve, depois incrementa
+Console.WriteLine("Pós Incremento");
+Console.WriteLine();
+
+int x = 5;
+
+Console.WriteLine($"Valor de x: {x}");
+Console.WriteLine($"Valor de x pós incremento: {x++}");
+Console.WriteLine($"Valor de x: {x}");
+Console.WriteLine();
+
+//pré incremento: primerio incrementa, depois resolve
+Console.WriteLine("Pré Incremento");
+Console.WriteLine();
+
+int y = 5;
+
+Console.WriteLine($"Valor de y: {y}");
+Console.WriteLine($"Valor de y pré incremento: {++y}");
+Console.WriteLine($"Valor de y: {y}");
+Console.WriteLine();
+
+//pós decremento: primeiro resolve, depois decrementa
+Console.WriteLine("Pós Decremento");
+Console.WriteLine();
+
+int z = 5;
+
+Console.WriteLine($"Valor de z: {z}");
+Console.WriteLine($"Valor de z pós decremento: {z--}");
+Console.WriteLine($"Valor de z: {z}");
+Console.WriteLine();
+
+//pré decremento: primeiro decrementa, depois resolve
+Console.WriteLine("Pré Decremento");
+Console.WriteLine();
+
+int w = 5;
+
+Console.WriteLine($"Valor de w: {w}");
+Console.WriteLine($"Valor de w pré decremento: {--w}");
+Console.WriteLine($"Valor de w: {w}");
+Console.WriteLine();
+
+Console.WriteLine("Pressione qualquer tecla para finalizar...");
+
+Console.ReadKey();
---
+++ 01_Fundamentos/20OperadoresRelacionais/Program.cs
@@ -0,0 +1,57 @@
+﻿/*
+ * Operadores Relacionais
+ * 
+ * == : igualdade
+ * >   : maior que
+ * <   : menor que
+ * >= : maior ou igual
+ * <= : menor ou igual
+ * !=  : diferente
+ * 
+ * Obeject.Equals: determina se 2 objetos são iguais
+ * 
+ * O resultado de uma operação relacional é TRUE ou FALSE
+ * 
+ * Referência 01: https://learn.microsoft.com/pt-br/dotnet/csharp/language-reference/operators/
+ * Referência 02: https://learn.microsoft.com/pt-br/dotnet/csharp/language-reference/operators/comparison-operators
+ * Referência 03: https://learn.microsoft.com/pt-br/dotnet/csharp/language-reference/operators/equality-operators
+ * 
+ */
+
+using System.Diagnostics.SymbolStore;
+
+Console.WriteLine("Operadores Relacionais");
+Console.WriteLine();
+
+int a = 15;
+int b = 25;
+bool comparacao = a == b;
+
+Console.WriteLine($"Resultado da comparação de igualdade: {comparacao}");
+Console.WriteLine();
+
+int x = 10;
+double y = 10;
+
+//operadores relacionais com tipo-valor
+Console.WriteLine($"o valor de x é igual ao valor de y: {x == y}");
+Console.WriteLine($"o objeto x é igual ao objeto y: {x.Equals(y)}");
+Console.WriteLine($"o valor de x é maior que o valor de y: {x > y}");
+Console.WriteLine($"o valor de x é menor que o valor de y: {x < y}");
+Console.WriteLine($"o valor de x é maior ou igual ao valor de y: {x >= y}");
+Console.WriteLine($"o valor de x é menor ou igual ao valor de y: {x <= y}");
+Console.WriteLine($"o valor de x é diferente do valor de y: {x != y}");
+Console.WriteLine();
+
+//operadores relacionais com tipo-referência
+
+string curso = "Curso de CSharp";
+string cursoAtualizado = "curso de csharp";
+
+Console.WriteLine($"A variável 'curso' é igual à variável 'cursoAtualizado (==)': {curso == cursoAtualizado}");
+Console.WriteLine($"A variável 'curso' é igual à variável 'cursoAtualizado' (Equals): {curso.Equals(cursoAtualizado)}");
+Console.WriteLine();
+
+Console.WriteLine("Pressione qualquer tecla para finalizar...");
+
+Console.ReadKey();
